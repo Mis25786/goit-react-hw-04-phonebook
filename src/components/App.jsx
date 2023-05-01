9e4d2adc793a5b6e807ec4c69015c7195071ca25
Mis25786 <111245815+Mis25786@users.noreply.github.com>
2023-05-01 18:12:22 +0200

    react-hw-4-phone-arry-zero

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,15 +9,9 @@ import ContactList from './ContactList';
 import css from './App.module.css';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState([]);
   const [filter, setFilter] = useState('');
   const isFirstRender = useRef(true);
-  console.log('isFirstRender :>> ', isFirstRender);
 
   const deleteContact = contactId => {
     setContacts(prevContacts =>
