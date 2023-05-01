29f7058bf81968f7ee469f7909efe0a1995d8e0b
Mis25786 <111245815+Mis25786@users.noreply.github.com>
2023-05-01 18:04:26 +0200

    reac-hw-4-phone-work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { nanoid } from 'nanoid';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
@@ -16,6 +16,8 @@ export const App = () => {
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
   ]);
   const [filter, setFilter] = useState('');
+  const isFirstRender = useRef(true);
+  console.log('isFirstRender :>> ', isFirstRender);
 
   const deleteContact = contactId => {
     setContacts(prevContacts =>
@@ -46,10 +48,7 @@ export const App = () => {
   };
 
   useEffect(() => {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-    console.log('contacts :>> ', contacts);
-    console.log('parsedContacts :>> ', parsedContacts);
+    const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
 
     if (parsedContacts) {
       setContacts(parsedContacts);
@@ -57,7 +56,16 @@ export const App = () => {
   }, []);
 
   useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+
+    const contactList = JSON.stringify(contacts);
+
+    if (contactList) {
+      localStorage.setItem('contacts', contactList);
+    }
   }, [contacts]);
 
   return (
@@ -79,95 +87,3 @@ export const App = () => {
     </>
   );
 };
-
-//=====================================================================
-
-// import { Component } from 'react';
-// import { nanoid } from 'nanoid';
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
-// import ContactForm from './ContactForm';
-// import Filter from './Filter';
-// import ContactList from './ContactList';
-
-// import css from './App.module.css';
-
-// export class App extends Component {
-//   state = {
-//     contacts: [
-//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//     ],
-//     filter: '',
-//   };
-
-//   deleteContact = contactId => {
-//     this.setState(prevState => ({
-//       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-//     }));
-//   };
-
-//   createUser = data => {
-//     this.setState(prevState => {
-//       if (prevState.contacts.find(contact => contact.name === data.name)) {
-//         return Notify.info('This name already exists in the list');
-//       }
-//       return { contacts: [...prevState.contacts, { ...data, id: nanoid() }] };
-//     });
-//   };
-
-//   cangeFilter = e => {
-//     this.setState({ filter: e.target.value });
-//   };
-
-//   getVisibleContacts = () => {
-//     const normalizeFilterContact = this.state.filter.toLowerCase();
-
-//     return this.state.contacts.filter(contact =>
-//       contact.name.toLowerCase().includes(normalizeFilterContact)
-//     );
-//   };
-
-//   componentDidMount() {
-//     const contacts = localStorage.getItem('contacts');
-//     const parsedContacts = JSON.parse(contacts);
-//     console.log('parsedContacts :>> ', parsedContacts);
-
-//     if (parsedContacts) {
-//       this.setState({ contacts: parsedContacts });
-//     }
-//   }
-
-//   componentDidUpdate(prevProps, prevState) {
-//     if (this.state.contacts !== prevState.contacts) {
-//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-//     }
-//   }
-
-//   render() {
-//     const { filter } = this.state;
-
-//     const visibleContacts = this.getVisibleContacts();
-
-//     return (
-//       <>
-//         <div className={css.phonebook}>
-//           <h1 className={css['phonebook-title']}>Phonebook</h1>
-
-//           <ContactForm createUser={this.createUser} />
-
-//           <h2>Contacts</h2>
-
-//           <Filter filter={filter} cangeFilter={this.cangeFilter} />
-
-//           <ContactList
-//             contacts={visibleContacts}
-//             onDeleteContact={this.deleteContact}
-//           />
-//         </div>
-//       </>
-//     );
-//   }
-// }
